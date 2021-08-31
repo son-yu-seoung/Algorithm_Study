1f2ebf514a8922e33c680a851096c98b8f9e7992
son-yu-seoung <thsdbtjd10@naver.com>
2021-08-31 23:46:48 +0900

    ☆

---
+++ Python Algorithm Interview/2. Reverse String.py
@@ -0,0 +1,25 @@
+# 344. Reverse String
+# Q. Write a function that reverse a string. The input string is given as an array of characters s.
+
+# Example 1:
+# Input : s = ["h", "e", "l", "l", "o"]
+# Output : ["o", "l", "l", "e", "h"]
+
+# Example 2:
+# Input : s = ["H", "a", "n", "n", "a", "h"]
+# Output : ["h", "a", "n", "n", "a", "H"]
+
+# My solution
+def reverseString(self, s: List[str]) -> None:
+    s[::1] = s[::-1]
+
+# Other solution 1
+def reverseString(self, s: List[str]) -> None:
+    left, right = 0, len(s)-1
+    s[left], s[right] = s[right], s[left]
+    left += 1
+    right += 1
+
+# Other solution 2
+def reverseString(self, s: List[str]) -> None:
+    s.reverse()
