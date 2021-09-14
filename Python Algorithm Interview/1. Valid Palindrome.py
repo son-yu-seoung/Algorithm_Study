95c507d2e6f524d44da2f2dee440d0fc8b4a800e
son-yu-seoung <65440674+son-yu-seoung@users.noreply.github.com>
2021-09-14 18:49:29 +0900

    [Easy] ☆

--- Python Algorithm Interview/1. Valid Palindrome.py
+++ Python Algorithm Interview/1. Valid Palindrome.py
@@ -1,6 +1,6 @@
 # 125. Valid Palindrome
 # Q. Given a string s, determine if it is a palindrome, 
-# considering only alphanumeric characters and ignoring cases.
+# considering only alphanumeric characters and ignoring cases. 
 
 # Example 1:
 # Input: s = "A man a plan, a canal: Panama"
