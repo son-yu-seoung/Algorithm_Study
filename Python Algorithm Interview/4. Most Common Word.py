a0fe64d7d71ff9e4b1b0667621ac0f923943dbc5
son-yu-seoung <65440674+son-yu-seoung@users.noreply.github.com>
2021-09-14 18:50:26 +0900

    [Easy] ★

--- Python Algorithm Interview/4. Most Common Word.py
+++ Python Algorithm Interview/4. Most Common Word.py
@@ -11,7 +11,7 @@
 # "ball" occurs twice (and no other word does), so it is the most frequent non-banned word in the paragraph. 
 # Note that words in the paragraph are not case sensitive,
 # that punctuation is ignored (even if adjacent to words, such as "ball,"), 
-# and that "hit" isn't the answer even though it occurs more because it is banned.
+# and that "hit" isn't the answer even though it occurs more because it is banned. 
 
 # Example 2 :
 
