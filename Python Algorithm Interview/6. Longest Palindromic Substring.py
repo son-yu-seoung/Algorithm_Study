39f31358a0a85686e7140e9756802ad7ab1d5942
son-yu-seoung <thsdbtjd10@naver.com>
2021-09-17 21:49:11 +0900

    [Medium] ★★☆

--- Python Algorithm Interview/6. Longest Palindromic Substring.py
+++ Python Algorithm Interview/6. Longest Palindromic Substring.py
@@ -17,9 +17,36 @@
 # Example 4:
 # Input : s = "ac"
 # Output : "a"
-s = 'abcde' 
 
-for i in range(len(s), 0, -1): 
-    for j in range(0, len(s) - i + 1):
-        if s[j:j+i] == s[j:j+i][::-1]: # s[0:5:1] == s[4:-1:-1]
-            print('t',s[j:j+i]) 
+# My solution -> Time Limit Excceded
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        for i in range(len(s), 0, -1): 
+            for j in range(0, len(s) - i + 1):
+                if s[j:j+i] == s[::-1][j:j+i]: # s[0:5:1] == s[4:-1:-1]
+                    return s[j:j+i]
+
+# Optimal solution
+class Solution:
+    def longestPalindrome(self, s: str) -> str:
+        def expand(left: int, right: int) -> str:
+            while left >= 0 and right < len(s) and s[left] == s[right]:
+                left -= 1
+                right += 1
+            return s[left+1:right]
+        
+        if len(s) < 2 or s == s[::-1]:
+            return s
+        
+        result = ''
+        
+        for i in range(len(s)-1):
+            result = max(result,
+                         expand(i, i+1),
+                         expand(i, i+2),
+                         key=len)
+        
+        return result
+        
+
+
